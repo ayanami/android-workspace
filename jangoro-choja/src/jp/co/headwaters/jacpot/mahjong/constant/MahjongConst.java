b33f4594efe4645c0bd93ca5409aa11eec842047
yoshimasa.suzuki <y_suzuki@headwaters.co.jp>
2013-08-13 23:34:29 +0900

    定数クラスを追加

---
+++ jangoro-choja/src/jp/co/headwaters/jacpot/mahjong/constant/MahjongConst.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package jp.co.headwaters.jacpot.mahjong.constant;
+
+
+/**
+ * <p>
+ * 麻雀定数クラスです。
+ * </p>
+ * 
+ * 作成日：2013/08/13<br>
+ * 
+ * <b>更新履歴</b><br>
+ * <table border bgcolor="#ffffff">
+ * <tr bgcolor="#ccccff">
+ * <td>日付</td>
+ * <td>欠陥管理番号</td>
+ * <td>担当</td>
+ * <td>変更点</td>
+ * </tr>
+ * <tr>
+ * <td>2013/08/13</td>
+ * <td>－</td>
+ * <td>HWS 鈴木</td>
+ * <td>新規作成</td>
+ * </tr>
+ * </table>
+ * 
+ * @author HWS 鈴木
+ */
+public class MahjongConst {
+
+    /** テキスト(門前自摸) */
+    public static final String SELF_DRAW = "門前自摸 1翻";
+    
+    /** テキスト(平和) */
+    public static final String ALL_RUNS = "平和 1翻";
+    
+    /** テキスト(ドラ) */
+    public static final String DRAGON = "ドラ";
+
+    /** テキスト(役牌) */
+    public static final String VALUE_TILES = "役牌";
+
+    /**
+     * 
+     * 定数クラスのため、コンストラクタをプロテクトします。
+     */
+    protected MahjongConst() {
+        
+    }
+}
